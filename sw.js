26c8a15eab4f638da869689a2052308236d2627d
francosealvarez02 <francosealvarez@gmail.com>
2026-09-01 12:58:52 -0300

    Design: estilo Apple / iOS Minimalista limpio con tarjetas redondeadas y tipografia Inter

--- sw.js
+++ sw.js
@@ -1,9 +1,9 @@
-﻿const CACHE_NAME = 'finanzas-pwa-v7';
+﻿const CACHE_NAME = 'finanzas-pwa-v8';
 const ASSETS = [
   './manifest.json',
   './icon-192.png',
   './icon-512.png',
-  'https://fonts.googleapis.com/css2?family=JetBrains+Mono:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;1,400&family=Space+Grotesk:wght@400;500;600;700&display=swap',
+  'https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800&display=swap',
   'https://cdnjs.cloudflare.com/ajax/libs/Chart.js/4.4.1/chart.umd.min.js',
   'https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.5.1/jspdf.umd.min.js',
   'https://cdnjs.cloudflare.com/ajax/libs/jspdf-autotable/3.8.2/jspdf.plugin.autotable.min.js',
